f2296bb9ab48440aea167fb5ce4c68c139a924d4
Abdussalam <abdussalamiysa@gmail.com>
2022-04-13 15:40:56 -0700

    completed all tasks

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * a function that prints lowercase alphabets, followed by a newline
+ */
+
+void print_alphabet(void)
+	{
+		char ch;
+		for (ch='a'; ch <= 'z'; ch++){
+			_putchar(ch);}
+		_putchar('\n');
+		return (0);
+	}
+
